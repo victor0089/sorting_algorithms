ea99f67c1ce97d6f4354334758548ce9738de81d
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-09-26 16:10:17 +0300

    Add files via upload

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,44 +1,43 @@
 #include "sort.h"
-
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-
-void swap(int *array, int item0, int item1)
+ * swap_ints - Swap two integersinanarray.
+ * @a: The firstntegertoswap.
+ * @b: The second integeroswap.
+ */
+void swap_ints(int *a, int *b)
 {
-
-
-	int temp;
-	temp = array[item0];
-	array[item0] = array[item1];
-	array[item1] = temp;
-
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
+
 /**
- * shell_sort - function that sorts an array ascending order
- * using the kneuth sequence
- * @size: array size
- * @array: list with numbers
+ * shell_sort - Sortanarrayofintegersi ascending
+ *              order using the shell sort algorithm.
+ * @array: An arrayofintegers.
+ * @size: The sizeofthearray.
+ *
+ * Description: Uses theKnuthntervalsequence.
  */
 void shell_sort(int *array, size_t size)
-
 {
-	size_t gap = 1, i, index = 0;
-	if( array == NULL || size < 2)
-		return;
-	while (gap < size / 3)
-		gap = 3 * gap + 1;
-	while (gap >= 1)
-	
-	{
-		for (i = gap; i < size; i++)
-			for (index = i; index >= gap &&
-					(array[index]< array[index - gap]);index -= gap)
-				swap(array, index, index - gap);
-		print_array(array, size);
-		gap /= 3;
-	}
+	size_t gp, i, j;
+if (array == NULL || size < 2)
+return;
+for (gp = 1; gp < (size / 3);)
+gp = gp * 3 + 1;
+for (; gp >= 1; gp /= 3)
+{
+for (i = gp; i < size; i++)
+{
+j = i;
+while (j >= gp && array[j - gp] > array[j])
+{
+swap_ints(array + j, array + (j - gp));
+j -= gp;
+}
+}
+print_array(array, size);
+}
 }
